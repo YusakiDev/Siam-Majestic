b3d7fccd77248fda9d1d3ff7bc3b7769d299a176
Jirateepatom <Jirateep.tana@bumail.net>
2022-07-02 17:54:57 +0700

    Menu
    LocalGameMenuset

--- Assets/Scripts/CharacterSelectionPlayer2.cs
+++ Assets/Scripts/CharacterSelectionPlayer2.cs
@@ -1,72 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class CharacterSelecetion : MonoBehaviour
+public class CharacterSelectionPlayer2 : MonoBehaviour
 {
-    private GameObject[] characterList;
+    private GameObject[] character2List;
     private int index;
 
     private void Start()
     {
         index = PlayerPrefs.GetInt("CharacterSelected");
             
-        characterList = new GameObject[transform.childCount];
+        character2List = new GameObject[transform.childCount];
         
         //fill the array with our model
         for (int i = 0; i < transform.childCount; i++)
         {
-            characterList[i] = transform.GetChild(i).gameObject;
+            character2List[i] = transform.GetChild(i).gameObject;
         }
 
         //we toogle off their renderer so we don't see them
-        foreach (GameObject go in characterList)
+        foreach (GameObject go in character2List)
         {
             go.SetActive(false);
         }
         
         //we toggle on the selected character
-        if (characterList[index])
+        if (character2List[index])
         {
-            characterList[index].SetActive(true);
+            character2List[index].SetActive(true);
         }
     }
 
     public void ToggleLeft()
     {
         //Toggle off the current model
-        characterList[index].SetActive(false);
+        character2List[index].SetActive(false);
         
         index--;
         if (index < 0)
         {
-            index = characterList.Length - 1;
+            index = character2List.Length - 1;
         }
         
         //Toggle on the ew model
-        characterList[index].SetActive(true);
+        character2List[index].SetActive(true);
     }
     public void ToggleRight()
     {
         //Toggle off the current model
-        characterList[index].SetActive(false);
+        character2List[index].SetActive(false);
         
         index++;
-        if (index == characterList.Length)
+        if (index == character2List.Length)
         {
             index = 0;
         }
         
         //Toggle on the ew model
-        characterList[index].SetActive(true);
+        character2List[index].SetActive(true);
     }
 
     //Change Scene Edit Scene Name here Dude
     public void ConfirmButton()
     {
         PlayerPrefs.SetInt("CharacterSelected", index);
-        SceneManager.LoadScene("CharacterSelectionPlayer2");
+        SceneManager.LoadScene("PlayScene");
     }
 }
