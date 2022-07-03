fd1811db5ef3fdf9791e4e65852c4e30322310a1
Jirateepatom <Jirateep.tana@bumail.net>
2022-07-03 13:43:23 +0700

    Market
    Buy soilder

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,52 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UI;
+
+
 public class ShopManager : MonoBehaviour
 {
+    
+    
+    [SerializeField] Button button3;
+    [SerializeField] Button button5;
+    [SerializeField] private TMP_Text _text;
 
-    public int[,] shopItems = new int[5,5];
+    [SerializeField] private int sum;
+    [SerializeField] private int coins = 2000;
 
-    public float coins;
-
-    public TextMeshProUGUI CoinsTXT;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        CoinsTXT.text = "Coins:" + coins.ToString();
-
-        //ID's
-        shopItems[1, 1] = 1;
-        shopItems[1, 2] = 2;
-        //price
-        shopItems[2, 1] = 3;
-        shopItems[3, 2] = 5;
-
-        //quantity //store solider data
-        shopItems[3, 1] = 0;
-        
-
-
+        button3.onClick.AddListener(Button3);
+        button5.onClick.AddListener(Button5);
     }
 
-    // Update is called once per frame
-    public void Buy()
+    private void Button3()
     {
-        //call button ref to use in this function
-        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
-
-        if (coins >= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        if (coins >= 3)
         {
-            coins -= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
-            shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID]+=5;
-            CoinsTXT.text = "Coins:" + coins.ToString();
-            ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text =
-                shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
-            
+            coins -= 3;
+            sum += 5;
+            _text.text = $"Coins: {coins} Sum: {sum}";
         }
 
-
+        else
+        {
+            Debug.Log("Not enough Money");
+        }
+    }
+    
+    private void Button5()
+    {
+        if (coins >= 5)
+        {
+            coins -= 5;
+            sum += 10;
+            _text.text = $"Coins: {coins} Sum: {sum}";
+        }
+        else
+        {
+            Debug.Log("Not enough Money");
+        }
     }
 }
